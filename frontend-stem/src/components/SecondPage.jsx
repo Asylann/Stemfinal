f5e5dbb04a066aa324337538eb8b00e709fe7bb8
Магжан <maqa25125@gmail.com>
2026-06-09 12:37:28 +0500

    SecondPage.jsx fix

--- frontend-stem/src/components/SecondPage.jsx
+++ frontend-stem/src/components/SecondPage.jsx
@@ -11,14 +11,14 @@ export default function SecondPage() {
     { title: t.furniture_kreslo, img: '/img/pagesecond/738d1eff.png', path: '/secondpage/kreslo' },
     { title: t.furniture_kuhnya, img: '/img/pagesecond/4a950c62bed144.jpg', path: '#' },
     { title: t.furniture_pufy, img: '/img/pagesecond/a41bcac159.png', path: '/secondpage/pufy' },
-    { title: t.furniture_stellazhi,img: '/img/page Second/e0f5951d3c3.png', путь: '/ Secondpage/stellazhi' },
+    { title: t.furniture_stellazhi,img: '/img/page Second/e0f5951d3c3.png', path: '/ Secondpage/stellazhi' },
     { title: t.furniture_stoly, img: '/img/pagesecond/bb20aa.png', path: '#' },
     { title: t.furniture_stulya, img: '/img/pagesecond/983793aa0.png', path: '/secondpage/stulya' },
     { title: t.furniture_tumby, img: '/img/pagesecond/4d735992.png', path: '/secondpage/tumby' },
-    { title: t.furniture_shkafy, img: '/img/page Second/ce38f5332a.png', путь: '/ Secondpage/shkafy' },
+    { title: t.furniture_shkafy, img: '/img/page Second/ce38f5332a.png', path: '/ Secondpage/shkafy' },
   ]
 
-  возвращаться (
+  return (
     <div className="second-page">
       <nav className="second-breadcrumb">
         <Link to="/" className="breadcrumb-link">{t.home}</Link>
